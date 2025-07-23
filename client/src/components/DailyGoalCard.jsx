8b1ce0a801611b741cd1025e037a92d66048a048
Avantheka Shrinivas <avanthekashrinivas@gmail.com>
2025-07-23 12:56:26 +0530

    Updated dashboard UI

--- client/src/components/DailyGoalCard.jsx
+++ client/src/components/DailyGoalCard.jsx
@@ -3,20 +3,23 @@ import "../components/Auth.css";
 
 const DailyGoalCard = () => {
   return (
-    <div className="goal-card">
-      <h3>Today's Fitness Goals</h3>
-      <ul>
-        <li>
+      <div className="goal-card">
+        <div className= "goal-container">
+          <h3>Today's Fitness Goals</h3>
+          </div>
+        <ul>
+          <li>
           💧 Water Intake Goal: <strong>3L</strong> <span className="status">✅</span>
-        </li>
-        <li>
+          </li>
+          <li>
           🏃 Cardio: <strong>30 min</strong> <span className="status">❌</span>
-        </li>
-        <li>
+          </li>
+          <li>
           👟 Steps: <strong>10,000</strong> <span className="status">✅</span>
-        </li>
-      </ul>
-    </div>
+          </li>
+        </ul>
+      </div>
+    
   );
 };
 
